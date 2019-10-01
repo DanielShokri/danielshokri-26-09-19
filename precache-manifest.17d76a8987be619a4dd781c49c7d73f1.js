da01b8e5f7c2b076c416ede7b2fc40cb5f340cfd
Daniel Shokri <danielshokri@gmail.com>
2019-10-01 21:19:33 +0300

    Updates

---
+++ precache-manifest.17d76a8987be619a4dd781c49c7d73f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e258d4a35850f223a9937788c4e8ff",
+    "url": "/danielshokri-26-09-19/index.html"
+  },
+  {
+    "revision": "6b705acea1588b3d5ea4",
+    "url": "/danielshokri-26-09-19/static/css/main.dd14bc76.chunk.css"
+  },
+  {
+    "revision": "9df566b613236347f541",
+    "url": "/danielshokri-26-09-19/static/js/2.11e89a8f.chunk.js"
+  },
+  {
+    "revision": "6b705acea1588b3d5ea4",
+    "url": "/danielshokri-26-09-19/static/js/main.ca1f05df.chunk.js"
+  },
+  {
+    "revision": "344ef2af0d892ac6ed2f",
+    "url": "/danielshokri-26-09-19/static/js/runtime-main.c5d38975.js"
+  }
+]);
